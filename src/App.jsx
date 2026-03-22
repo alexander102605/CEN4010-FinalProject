571732c89601d72a62d8a7712e5ef746844a5a80
Alexander Horan <ahoran2020@fau.edu>
2026-03-22 12:52:42 -0400

    Finished first proper iteration of the filtering function

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import "./App.css";
+import { useState, useEffect } from 'react';
+import ipa from './assets/data.json' with {type: 'json'};
+
+
 
 
 const App = () => {
+  const [filters, setFilters] = useState([]);
+  const [chars, setChars] = useState([]);
+  
+useEffect(() => {setFilters([{"type": "consonant"}, {"type": "vowel"}])}, []);
+  
+
+  const applyFilters = (filters, data) => {
+    let result = [];
+      for (let i of filters) {
+        let temp = data.filter((item) => item[Object.keys(i)] == Object.values(i))
+        result.push(...temp)
+      }
+      setChars(result)
+  }
+
   return (
     <div>
-      
+      test
+      <button onClick={() => applyFilters(filters, ipa)}>test button</button>
+      <button onClick={() => console.log(chars)}>print chars</button>
     </div>
   );
 }
